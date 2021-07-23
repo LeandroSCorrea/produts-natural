b4c2ac6c16918b988971505af1254db3c058c9a1
LeandroSCorrea <scleandro@gmail.com>
2021-07-23 16:24:42 -0300

    terceiro commit

--- js.js
+++ js.js
@@ -1,14 +1,20 @@
 function checa_formulario(form) {
   var mensagem = document.getElementById('mensagem')
-  //var email = document.getElementById('email')
   var invalid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/ //espressão regular
+  var email = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
 
   caixa_nome = document.querySelector('.msg-nome')
   caixa_nome.style.display = 'none' //desabilita a propriedade no HTML
 
+  caixa_email = document.querySelector('.msg-email')
+  caixa_email.style.display = 'none'
+
+  caixa_tel = document.querySelector('.msg-tel')
+  caixa_tel.style.display = 'none'
+
   if (form.name.value.length < 5) {
     //estamos validando pela propriedade 'name' do formulário
-    caixa_nome.innerHTML = 'Favor preencher seu nome'
+    caixa_nome.innerHTML = 'Favor preencher o nome'
     caixa_nome.style.display = 'block' //habilita o campo
 
     //alert('Digite o nome completo')
@@ -17,22 +23,28 @@ function checa_formulario(form) {
   }
 
   if (form.email.value == '') {
-    //
-    alert('E-mail não informado')
-    email.focus()
+    caixa_email.innerHTML = 'Favor preencher o email'
+    caixa_email.style.display = 'block'
+
+    //alert('E-mail não informado')
+    form.email.focus()
     return false
   }
 
   if (invalid.test(form.email.value) == false) {
-    alert('Endereço de e-mail inválido')
+    caixa_email.innerHTML = 'Favor preencher o email válido'
+    caixa_email.style.display = 'block'
+
+    //alert('Endereço de e-mail inválido')
     form.email.focus()
     return false
   }
 
   if (form.tel.value == '') {
-    //estamos validando pela propriedade 'Id' do html
-    alert('Telefone não informado')
-    tel.focus()
+    caixa_tel.innerHTML = 'Favor preencher o telefone'
+    caixa_tel.style.display = 'block'
+    //alert('Telefone não informado')
+    form.tel.focus()
     return false
   }
 }
